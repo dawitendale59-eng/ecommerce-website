80bec25e27833b16b87fcd9d5ed3551250955246
Dawit Endale <dawitendale59@gmail.com>
2026-03-16 14:59:35 +0300

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,23 @@
+
+import './App.css'
+import { Route, Routes } from 'react-router-dom';
+import Auth from './pages/Auth';
+import Checkout from './pages/Checkout';
+import Home from './pages/Home';
+import Navbar from './components/Navbar';
+function App() {
+  
+  return( 
+  <div className="app">
+    <Navbar />
+    <Routes>
+      <Route path='/' element={<Home />} />
+  <Route path='/auth' element={<Auth />} />
+   <Route path='/checkout' element={<Checkout />} />
+  
+    </Routes>
+  </div>
+    );
+}
+
+export default App;
